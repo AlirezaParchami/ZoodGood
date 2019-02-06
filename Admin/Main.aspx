0772e06b3f1badc81837b02dd548863a8081fd44
Alireza <alirezaprm76@gmail.com>
2019-02-06 21:04:48 +0330

    Main in Admin page uploaded

---
+++ Admin/Main.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Admin_Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Admin main page
+        <br />
+        <asp:Button onclick="btn_add_goods" runat="server" Text="Add Goods" />
+        <asp:Button onclick="btn_add_category" runat="server" Text="Add Category" />
+        <asp:Button onclick="btn_confirm_cart" runat="server" Text="Confirm Cart" />
+        <asp:Button onclick="btn_confirm_comment" runat="server" Text="Confirm Comment" />
+        <asp:Button ID="btn_change_cat" runat="server" Text="Change category" OnClick="btn_change_cat_click" />
+        <asp:Button ID="btn_logout" runat="server" Text="LogOut" OnClick="btn_logout_click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Admin/Main.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_Main : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["User_ID"] == null || !Session["User_ID"].Equals("Admin")) // If it is not admin, go to SignIn page
+        {
+            Response.Redirect("../SignIn.aspx");
+        }
+    }
+
+    protected void btn_add_goods(object sender, EventArgs e)
+    {
+        Response.Redirect("AddGood.aspx");
+    }
+    protected void btn_add_category(object sender, EventArgs e)
+    {
+        Response.Redirect("AddCategory.aspx");
+    }
+    protected void btn_confirm_cart(object sender, EventArgs e)
+    {
+        Response.Redirect("req_confirm.aspx");
+    }
+    protected void btn_confirm_comment(object sender, EventArgs e)
+    {
+        Response.Redirect("ConfirmComment.aspx");
+    }
+
+    protected void btn_logout_click(object sender, EventArgs e)
+    {
+        Session["User_ID"] = "";
+        Response.Redirect("../SignIn.aspx");
+    }
+
+    protected void btn_change_cat_click(object sender, EventArgs e)
+    {
+        Response.Redirect("change_category.aspx");
+    }
+
+}
