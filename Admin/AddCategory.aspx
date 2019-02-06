4b3b7029cb2e04637cb231eee3ee9eedfe7208a6
Alireza <alirezaprm76@gmail.com>
2019-02-06 21:06:07 +0330

    Add Category

---
+++ Admin/AddCategory.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AddCategory.aspx.cs" Inherits="Admin_AddCategory" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="txt_name" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="btn_add" runat="server" Text="Add" OnClick="btn_add_click" />
+        <br />
+        <asp:Label ID="lbl_result" runat="server" Text=""></asp:Label>
+        <br />
+        <asp:Button ID="btn_back" runat="server" Text="Back" onclick="btn_back_click"/>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Admin/AddCategory.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_AddCategory : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["User_ID"] == null || !Session["User_ID"].Equals("Admin"))
+        {
+            Response.Redirect("../SignIn.aspx");
+        }
+    }
+    protected void btn_back_click(object sender, EventArgs e)
+    {
+        Response.Redirect("Main.aspx");
+    }
+    protected void btn_add_click(object sender, EventArgs e)
+    {
+        String connectionString = null;
+        String Query = null;
+
+        connectionString = "Server=localhost;" +
+                       "DataBase=ZoodGood;" +
+                       "Trusted_Connection=Yes;";
+
+        using (SqlConnection cnn = new SqlConnection(connectionString))
+        {
+            cnn.Open();
+
+            Query = "insert into Category (Name) values ('"  + txt_name.Text +  "')";
+
+            using (SqlCommand cmd = new SqlCommand(Query, cnn))
+            {
+                cmd.ExecuteNonQuery();
+                lbl_result.Text = txt_name.Text + " Category has been added.";
+                txt_name.Text = "";
+            }
+        }
+    }
+}
